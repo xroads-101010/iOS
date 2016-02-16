3f1aba7a859c7ae590cb9de07a6704fe1a7cb1c7
abinanto <ABIN@ABIN-MAC.local>
2016-02-16 13:12:47 +0530

    Initial Commit

---
+++ xroads/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xroads
+//
+//  Created by Abin Anto on 16/02/16.
+//  Copyright (c) 2016 lister. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
