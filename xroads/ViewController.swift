917706c477d74531392189e2cbf6756e49e26d2a
AbdulSakeer <sakeer.smile@gmail.com>
2016-02-25 12:53:08 +0530

    Removed the unused Launch variable

--- xroads/ViewController.swift
+++ xroads/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let Launch = self.storyboard!.instantiateViewControllerWithIdentifier("LaunchViewController") as! LaunchViewController
+        /*let Launch =*/ self.storyboard!.instantiateViewControllerWithIdentifier("LaunchViewController") as! LaunchViewController
         
         //self.navigationController!.pushViewController(Launch, animated: true)
     }
