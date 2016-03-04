2ecdbbdcbc12ffcad558994aaec7d7bcabb34b03
AbdulSakeer <sakeer.smile@gmail.com>
2016-03-04 13:15:11 +0530

    Displaying the json details in table view by hitting the server

--- xroads/Trip.swift
+++ xroads/Trip.swift
@@ -6,16 +6,49 @@
 //  Copyright © 2016 lister. All rights reserved.
 //
 
-import UIKit
+/*import UIKit
 
 struct Trip {
     var tripName: String?
     var tripId: Int
     var tripStartPlace: String?
     
-    init(tripName: String?, tripId: Int, tripStartPlace: String?) {
+    /*init(tripName: String?, tripId: Int, tripStartPlace: String?) {
         self.tripName = tripName
         self.tripId = tripId
         self.tripStartPlace = tripStartPlace
+    }*/
+    
+    init(json: NSDictionary) {
+        self.tripName = json["name"] as? String
+        self.tripId = 005
+        self.tripStartPlace = json["html_url"] as? String
+    }
+}*/
+
+
+import UIKit
+
+class Trip {
+    
+    var tripName: String?
+    var tripDestination: String?
+    var tripChampion: String?
+    var hasTripStarted: String?
+    var startTime: String?
+    var endTime: String?
+    var createdAt: String?
+    var updatedAt: String?
+    
+    init(json: NSDictionary)
+    {
+        self.tripName = json["tripName"] as? String
+        self.tripDestination = json["tripDestination"] as? String
+        self.tripChampion = json["tripChampion"] as? String
+        self.hasTripStarted = json["hasTripStarted"] as? String
+        self.startTime = json["startTime"] as? String
+        self.endTime = json["endTime"] as? String
+        self.createdAt = json["createdAt"] as? String
+        self.updatedAt = json["updatedAt"] as? String
     }
 }
--- xroads/TripsTableViewController.swift
+++ xroads/TripsTableViewController.swift
@@ -12,8 +12,7 @@ class TripsTableViewController: UITableViewController {
 
     @IBOutlet var menuButton: UIBarButtonItem!
     
-    var trips:[Trip] = tripData
-    
+    var TableData = [Trip]()
    
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +27,10 @@ class TripsTableViewController: UITableViewController {
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        get_data_from_url("https://4ef93830.ngrok.io/xroads-app/trip/champion?id=4");
+        
+        //makeHTTPGetRequest("https://4ef93830.ngrok.io/xroads-app/trip/champion?id=4")
     }
 
     @IBAction func menuButtonClick(sender: UIBarButtonItem) {
@@ -47,26 +50,105 @@ class TripsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return trips.count
+        return TableData.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("TripCell", forIndexPath: indexPath)
         
-        let trip = trips[indexPath.row] as Trip
+        let trip = TableData[indexPath.row]
+        
         cell.textLabel?.text = trip.tripName
+        
+        //cell.textLabel?.text = TableData[indexPath.row].html_url
+        
         //cell.detailTextLabel?.text = player.game
+        
         return cell
     }
     
     override func viewWillAppear(animated: Bool) {
         
-        self.tabBarController?.navigationItem.title = "Upcoming Events"
+        self.tabBarController?.navigationItem.title = "Upcoming Trips"
         self.tabBarController?.navigationItem.leftBarButtonItem = menuButton
         
     }
     
+    func get_data_from_url(url:String)
+    {
+        
+        if let JSONData = NSData(contentsOfURL: NSURL(string: url)!)
+        {
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(JSONData, options: NSJSONReadingOptions()) as? NSDictionary
+                
+                if let reposArray = json!["trips"] as? [NSDictionary] {
+                    
+                    for trip in reposArray {
+                        TableData.append(Trip(json: trip))
+                    }
+                }
+                
+                print(json)
+            } catch {
+                print("error serializing JSON: \(error)")
+            }
+            
+        }
+    }
+    
+    func makeHTTPGetRequest(path: String)
+    {
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {
+            (data, response, error) in
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            guard error == nil else {
+                print("error calling GET on /posts/1")
+                print(error)
+                return
+            }
+            
+            // parse the result as JSON, since that's what the API provides
+            let json: NSDictionary
+            do {
+                json = try NSJSONSerialization.JSONObjectWithData(responseData,
+                    options: NSJSONReadingOptions()) as! NSDictionary
+                
+                if let reposArray = json["trips"] as? [NSDictionary] {
+                    
+                    for trip in reposArray {
+                        self.TableData.append(Trip(json: trip))
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+
+            } catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
+            // now we have the post, let's just print it to prove we can access it
+            print("The post is: " + json.description)
+            
+            // the post object is a dictionary
+            // so we just access the title using the "title" key
+            // so check for a title and print it if we have one
+            if let postTitle = json["title"] as? String {
+                print("The title is: " + postTitle)
+            }
+        })
+        task.resume()
+    }
+
 
     /*
     // Override to support conditional editing of the table view.
