066b220adabfada07b038b29b509916261bb63c9
abinanto <ABIN@lt-mac-09.local>
2016-03-07 19:52:50 +0530

    commiting Google maps and images

--- xroads/AppDelegate.swift
+++ xroads/AppDelegate.swift
@@ -6,6 +6,7 @@
 // Testing
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyABS4N3_IGk_ZotKGH8GOMVDXHUUGpZ1jQ")
         return true
     }
 
